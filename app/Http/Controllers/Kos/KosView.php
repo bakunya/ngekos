31bc2fba5e48c5e14562eca5428a23aae80a6409
irvan hakim <irvanhakim.dev@gmail.com>
2023-05-23 23:30:57 +0700

    test CD action github

---
+++ app/Http/Controllers/Kos/KosView.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Kos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KosView extends Controller
+{
+    public function index()
+    {
+        dd('this page under development!!');
+    }
+}
--- routes/for/manage-kos.php
+++ routes/for/manage-kos.php
@@ -2,11 +2,12 @@
 
 use App\Http\Controllers\Kos\KosFormAction;
 use App\Http\Controllers\Kos\KosFormView;
+use App\Http\Controllers\Kos\KosView;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/admin/kos')->group(function ()
 {
-    Route::get('/', [KosFormView::class, 'index'])->name("GET.admin.kos.index");
+    Route::get('/', [KosView::class, 'index'])->name("GET.admin.kos.index");
     Route::get('/create', [KosFormView::class, 'create'])->name("GET.admin.kos.create");
     Route::post('/create', [KosFormAction::class, 'create'])->name("POST.admin.kos.create");
 });
